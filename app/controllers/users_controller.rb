d8fe868f828868cbf47ec3296dbdc85ad1fa360a
AnthonyTC89 <ptonyptc@gmail.com>
2019-09-10 15:24:44 -0500

    four get actions created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
-  
+  def index
+    @name = "index section!"
+  end
+
+  def show
+    @name = "show section!"
+  end
+
+  def new
+    @name = "new section!"
+  end
+
+  def edit
+    @name = "edit section!"
+  end
+
+  def create
+    #@name = "create section"
+  end
 end
